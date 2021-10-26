beeb65124c217e45feb93060a68c9c6add169c0a
calebc <calebc1@protonmail.com>
2021-10-26 10:50:47 -0700

    began coinapi implementation

--- coinmonitor/coinapi/coinapi.go
+++ coinmonitor/coinapi/coinapi.go
@@ -1,2 +1,11 @@
 package coinapi
 
+const BaseURL = "https://rest-sandbox.coinapi.io/"
+
+type Client struct {
+	apiKey string
+}
+
+func New(apiKey string) *Client{
+	return &Client{apiKey: apiKey}
+}
--- coinmonitor/macd.go
+++ coinmonitor/macd.go
@@ -1,8 +1,4 @@
 package coinmonitor
 
 func (s *Monitor) macd() float64 { //return difference between 12-26 moving average and 9 moving average of 12-26 moving average. negative indicates sell positive indicates buy.
-	if assets, err := s.krakenapi.Assets(); err != nil {
-		panic(err)
-	}
-	fmt.Println(assets)
 }
